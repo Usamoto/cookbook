0b3ba4feb463b008c7e78412cb979e341215c19d
Joss Doebler <mail@joss-doebler.de>
2020-07-03 22:29:07 +0200

    Added functionality to copy link to clipboard

--- buttons.js
+++ buttons.js
@@ -32,4 +32,38 @@ function hideModal() {
 
   // Just to be thorough, we also empty the value attribute of the input inside of the Modal, so it doesn't linger in the markup
   document.getElementById('share-link').setAttribute('value', '');
+
+  // And we also set the clipboard-message to be hidden (see function below)
+  document.getElementById('clipboard-message').classList.add('hide');
+}
+
+// For the "Copy"-button, we want to copy the URL to the user's clipboard,
+// which is a little tricky but doable.
+function copyToClipboard() {
+  // First, we need the value that we want to copy, which is in the input element
+  const value = document.getElementById('share-link').getAttribute('value');
+
+  // We create a textarea element and set its content to the thing we want to copy
+  const el = document.createElement('textarea');
+  el.value = value;
+
+  // We make the textarea non-editable and hide it away out of sight
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+
+  // We add it to the page, so we can select it
+  document.body.appendChild(el);
+  el.select();
+
+  // We copy the value of the selection
+  document.execCommand('copy');
+
+  // We remove the textarea from the document
+  document.body.removeChild(el);
+
+  // Finally, we tell the user that we copied the value to their clipboard.
+  // For that, we have a message in our markup that is hidden by default.
+  // We simply remove the "hide"-class.
+  document.getElementById('clipboard-message').classList.remove('hide');
 }
